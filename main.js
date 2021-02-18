f57a160a81362a31d8ba801955f627304e730a42
teleguest <eskff23456789@gmail.com>
2021-02-18 19:24:06 +0100

    add project on project list in project.html

---
+++ main.js
@@ -0,0 +1,9 @@
+function slide1(){
+    var slider1image1 = document.getElementById("slider-pr1");
+    slider1image1.src = "https://teleguest.github.io/CubeGame_Website/image/cubegame-menu.png";
+}
+
+function slide2(){
+    var slider1image2 = document.getElementById("slider-pr1");
+    slider1image2.src = "https://teleguest.github.io/CubeGame_Website/image/cubegame-boss.png";
+}
