7d9c5c8d4190e36e21749b7a92b99803a7a13413
teleguest <56689167+teleguest@users.noreply.github.com>
2021-04-27 16:02:33 +0200

    Create tasks.js

---
+++ archive/tele-tasker/tasks.js
@@ -0,0 +1,9 @@
+new Vue({
+    el: "#app",
+    data: {
+        tasks: [
+            {name: "zrobić system komentarzy do OnionTube", status: "✖️", date: "[26.4.2021]"},
+            {name: "nagrać film o chinskiej konsoli", status: "✖️", date: "[26.4.2021]"}
+        ]
+    }
+})
