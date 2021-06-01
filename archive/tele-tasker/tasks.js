a6f8591e5d0419737ac9c4a08b5b277697e6d75a
tele <tele@pop-os.localdomain>
2021-06-01 20:35:27 +0200

    add new style

--- archive/tele-tasker/tasks.js
+++ archive/tele-tasker/tasks.js
@@ -3,7 +3,7 @@ new Vue({
     data: {
         tasks: [
             {name: "zrobić system komentarzy do OnionTube", status: "✖️", date: "[26.4.2021]"},
-            {name: "nagrać film o chinskiej konsoli", status: "✖️", date: "[26.4.2021]"}
+            {name: "nagrać film o chinskiej konsoli", status: "✖️", date: "[26.4.2021]"},
         ]
     }
 })
---
+++ js/slider.js
@@ -0,0 +1,8 @@
+const slider = document.getElementById('slider');
+
+function slide1(){
+    slider.src = "/img/cubegame-menu.png";
+}
+function slide2(){
+    slider.src = "/img/cubegame-boss.png";
+}
